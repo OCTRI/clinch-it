5a824e96ed78c180feaeda0936273fd92988c458
Amy Yates <yateam@ohsu.edu>
2019-02-25 15:20:38 -0800

    Basic files generated from archetype.

---
+++ src/main/java/org/octri/clinchit/SecurityConfiguration.java
@@ -0,0 +1,11 @@
+package org.octri.clinchit;
+
+
+/**
+ * Auto-generated class. This is only useful when using the OCTRI authentication-lib and may otherwise be deleted. If
+ * using the auth lib, this class should extend FormSecurityConfiguration.
+ *
+ */
+public class SecurityConfiguration {
+	
+}
